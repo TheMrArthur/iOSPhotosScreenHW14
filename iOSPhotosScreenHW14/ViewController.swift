f82e6b407f5c08fa315b3b314fa2bed90f31b37d
TheMrArthur <arturgorlov5@gmail.com>
2022-09-06 08:49:40 +0300

    Initial Commit

---
+++ iOSPhotosScreenHW14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSPhotosScreenHW14
+//
+//  Created by Артур Горлов on 06.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
